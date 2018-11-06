e25abc47c2123a50ad17a762b5b908f0121a7e03
Amoki <hugo.duroux@gmail.com>
2018-11-06 11:09:51 +0100

    Get ready for deployment

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:3.7
+WORKDIR /opt
+ADD doc_sphinx/requirements.txt /opt
+RUN pip install -r requirements.txt
+COPY ./ /opt
+RUN sphinx-build -b html doc_sphinx html_doc
+
+FROM nginx:stable-alpine
+COPY --from=0 /opt/html_doc/ /usr/share/nginx/html/
+COPY --from=0 /opt/redoc/ /usr/share/nginx/redoc/
--- doc_sphinx/conf.py
+++ doc_sphinx/conf.py
@@ -50,12 +50,6 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 
-from recommonmark.parser import CommonMarkParser
-
-# source_parsers = {
-#     '.md': CommonMarkParser,
-# }
-
 #source_suffix = ['.rst', '.md']
 source_suffix = '.rst'
 
@@ -203,7 +197,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static', '_redoc']
+html_static_path = ['_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
