21c702c4f89bc70bff2b9fe2b31de3e961038473
Sarah Haïm-Lubczanski <sarah@bimdata.io>
2018-12-12 17:13:47 +0100

    Modifying conf.py to clean it
    A sidebar removed

--- doc_sphinx/conf.py
+++ doc_sphinx/conf.py
@@ -156,17 +156,18 @@ html_theme_options = {
 }
 
 
-
+scv_priority = 'branches'
+scv_whitelist_branches = ('master','dev', 'tech-writing')
 
 html_style = 'css/my_theme.css' 
-html_file_suffix = None
 html_context = {
-  'display_github': True,
-  'github_user': 'bimdata',
-  'github_repo': 'documentation',
-  'github_branch': 'dev/doc_sphinx/'
+    'github_user': 'bimdata',
+    'github_repo': 'documentation',
+    'github_version': 'dev/',
+    'display_github': True,
+    'conf_py_path': 'doc_sphinx/',
+    'source_suffix': '.rst',
 }
-
 
 
 # The name of an image file (relative to this directory) to place at the top
@@ -227,7 +228,7 @@ html_use_smartypants = True
 #
 # html_sidebars = {}
 html_sidebars = {
-   '**': ['globaltoc.html','sourcelink.html', 'searchbox.html', 'side_versions.html'],
+   '**': ['globaltoc.html','sourcelink.html', 'searchbox.html'],
 
 }
 
