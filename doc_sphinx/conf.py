85afe67c346bbfed6acff0ada7179756ba9d3c38
Sarah Haïm-Lubczanski <sarah@bimdata.io>
2019-04-05 17:38:53 +0200

    Reduce error messages when building
    first part

--- doc_sphinx/conf.py
+++ doc_sphinx/conf.py
@@ -101,7 +101,7 @@ if HAS_ROBOTS_TXT == "true":
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "README.rst"]
 
 # The name of the Pygments (syntax highlighting) style to use.
 # pygments_style = 'sphinx'
