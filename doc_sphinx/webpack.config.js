cdd7678c163cd6894a38e1c93bc877c0da1176e8
Gaelle <gaelle@bimdata.io>
2019-06-27 13:32:35 +0200

    add webpack

---
+++ doc_sphinx/webpack.config.js
@@ -0,0 +1,72 @@
+const webpack = require("webpack");
+const path = require("path");
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+  entry: './_static/js/index.js',
+  output: {
+    filename: 'js/bundle.js',
+    path: path.resolve(__dirname, '../html_doc/_static')
+  },
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/style.css",
+      chunkFilename: "[id].css"
+    }),
+    new CopyPlugin([
+      { from: './node_modules/@bimdata/utils/dist/fonts', to: '../dist/fonts/' }
+    ])
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/'
+          }
+        }
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            name: ('img/[name].[hash:7].[ext]')
+          }
+        },
+        {
+          loader: 'file-loader'
+        }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader' }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: "css-loader" },
+          { loader: "resolve-url-loader" },
+          { loader: "sass-loader" }
+        ]
+      }
+    ]
+  }
+};
