403137ae634971e0cf13194d55c8e654af6e73d0
Gaelle <gaelle@bimdata.io>
2019-07-01 09:27:47 +0200

    test w/ new dist folder

--- doc_sphinx/webpack.config.js
+++ doc_sphinx/webpack.config.js
@@ -22,7 +22,8 @@ module.exports = {
       chunkFilename: "[id].css"
     }),
     new CopyPlugin([
-      { from: './node_modules/@bimdata/utils/dist/fonts', to: '../dist/fonts/' }
+      { from: './node_modules/@bimdata/utils/dist/fonts', to: '../dist/fonts/' },
+      { from: './node_modules/@bimdata/utils/dist/fonts', to: '../_static/dist/fonts/' }
     ])
   ],
   module: {
