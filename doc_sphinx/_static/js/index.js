00df094923a230d9ecb31e05af3871f67c823daf
Gaelle <gaelle@bimdata.io>
2019-07-09 11:50:54 +0200

    custom arrow && deploy and collapse menu doc API

--- doc_sphinx/_static/js/index.js
+++ doc_sphinx/_static/js/index.js
@@ -1,1 +1,22 @@
 import '../scss/style.scss';
+
+$(function () {
+  $(document).ready(function () {
+
+    $('a').click(function(){
+      var divId = $(this).attr('href');
+       $('html, body').animate({
+        scrollTop: $(divId).offset().top - 60
+      }, 100);
+    });
+
+    $("#sidebar #nav section a").click(function () {
+      $("#sidebar #nav section a.active").removeClass("active");
+      $(this).addClass("active");
+    });
+
+    $('.toctree-expand').on('click', function () {
+      $(this).closest('section').toggleClass('expand');
+    });
+  });
+});
